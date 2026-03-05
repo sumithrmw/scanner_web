88df6d83bc677a2748758c6efed67de0ad568fa9
Sumithh <sumithbaby1995@gmail.com>
2026-03-04 20:50:39 -0800

    Fix: vite config, html typo, vite version

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/bar_code_web/', // Replace with your repo name
+  base: '/scanner_web/', // Replace with your repo name
 })
